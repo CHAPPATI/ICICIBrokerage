7f21f1fd37b1d40da2486e4712f0db9433f3a8b2
athyanth <82070169+athyanth@users.noreply.github.com>
2021-04-11 20:18:05 +0530

    Updated for positional trading

--- js/main.js
+++ js/main.js
@@ -5,38 +5,77 @@ function CalculateBrokerage() {
         parseFloat(document.getElementById('SellQuantity').value) : 0;
     const sellPrice = document.getElementById('SellPrice').value ?
         parseFloat(document.getElementById('SellPrice').value) : 0;
-    if (buyQuantity && Number.isInteger(buyQuantity) && buyPrice && (sellPrice || sellPrice === 0)) {
-        document.getElementById('result').style.display = 'block'
-        const totBuyPrice = buyQuantity * buyPrice;
-        const totSellPrice = sellQuantity * sellPrice;
-        const buyBrokerage = (totBuyPrice > 55555.0) ?
-            RoundOffValue(totBuyPrice * 0.00027) :
-            (totBuyPrice > 600) ? 15 : RoundOffValue(totBuyPrice * 0.025);
-        const sellBrokerage = (totSellPrice > 55555.0) ?
-            RoundOffValue(totSellPrice * 0.00027) :
-            (totSellPrice > 600) ? 15 : RoundOffValue(totSellPrice * 0.025);
-        const totalBrokerage = buyBrokerage + sellBrokerage;
-        const payOut = totSellPrice - totBuyPrice;
-        const etc = RoundOffValue((totBuyPrice + totSellPrice) * 0.000034);
-        const sebi = RoundOffValue((totBuyPrice + totSellPrice) * 0.000001);
-        const gst = RoundOffValue((totalBrokerage + etc + sebi) * 0.09);
-        const totalGST = gst * 2;
-        const sd = RoundOffValue(totBuyPrice * 0.0000325);
-        const stt = RoundOffValue(totSellPrice * 0.00025);
-        const totalDed = RoundOffValue(totalBrokerage + etc + sebi + totalGST + stt + sd);
-        const netAmount = payOut - totalDed;
-        document.getElementById('PayOut').value = payOut;
-        document.getElementById('Brokerage').value = totalBrokerage;
-        document.getElementById('ETC').value = etc;
-        document.getElementById('SEBI').value = sebi;
-        document.getElementById('SGST').value = gst;
-        document.getElementById('CGST').value = gst;
-        document.getElementById('GST').value = totalGST;
-        document.getElementById('SD').value = sd;
-        document.getElementById('STT').value = stt;
-        document.getElementById('TotalDeduction').value = totalDed;
-        document.getElementById('NetAmount').value = netAmount;
-        document.getElementById('PayLabel').innerHTML = (netAmount > 0) ? 'Payout Obligation' : 'Pay in Obligation';
+    const isIntraday = document.getElementById('intraday').checked;
+    if(isIntraday){
+        if (buyQuantity && Number.isInteger(buyQuantity) && buyPrice && (sellPrice || sellPrice === 0)) {
+            document.getElementById('result').style.display = 'block'
+            const totBuyPrice = buyQuantity * buyPrice;
+            const totSellPrice = sellQuantity * sellPrice;
+            const buyBrokerage = (totBuyPrice > 55555.0) ?
+                RoundOffValue(totBuyPrice * 0.00027) :
+                (totBuyPrice > 600) ? 15 : RoundOffValue(totBuyPrice * 0.025);
+            const sellBrokerage = (totSellPrice > 55555.0) ?
+                RoundOffValue(totSellPrice * 0.00027) :
+                (totSellPrice > 600) ? 15 : RoundOffValue(totSellPrice * 0.025);
+            const totalBrokerage = buyBrokerage + sellBrokerage;
+            const payOut = totSellPrice - totBuyPrice;
+            const etc = RoundOffValue((totBuyPrice + totSellPrice) * 0.000034);
+            const sebi = RoundOffValue((totBuyPrice + totSellPrice) * 0.000001);
+            const gst = RoundOffValue((totalBrokerage + etc + sebi) * 0.09);
+            const totalGST = gst * 2;
+            const sd = RoundOffValue(totBuyPrice * 0.0000325);
+            const stt = RoundOffValue(totSellPrice * 0.00025);
+            const totalDed = RoundOffValue(totalBrokerage + etc + sebi + totalGST + stt + sd);
+            const netAmount = payOut - totalDed;
+            document.getElementById('NetAmount').value = netAmount;
+            document.getElementById('AmountRequired').value = RoundOffValue(totBuyPrice / 20.0);
+            document.getElementById('TotalDeduction').value = totalDed;
+            document.getElementById('PayLabel').innerHTML = (netAmount > 0) ? 'Payout Obligation' : 'Pay in Obligation';
+            document.getElementById('PayOut').value = payOut;
+            document.getElementById('Brokerage').value = totalBrokerage;
+            document.getElementById('ETC').value = etc;
+            document.getElementById('SEBI').value = sebi;
+            document.getElementById('SGST').value = gst;
+            document.getElementById('CGST').value = gst;
+            document.getElementById('GST').value = totalGST;
+            document.getElementById('SD').value = sd;
+            document.getElementById('STT').value = stt;
+        }
+    } else {
+        if (buyQuantity && Number.isInteger(buyQuantity) && buyPrice && (sellPrice || sellPrice === 0)) {
+            document.getElementById('result').style.display = 'block'
+            const totBuyPrice = buyQuantity * buyPrice;
+            const totSellPrice = sellQuantity * sellPrice;
+            const buyBrokerage = (totBuyPrice > 9259.0) ?
+                RoundOffValue(totBuyPrice * 0.0027) :
+                (totBuyPrice > 3333.33) ? 25 : RoundOffValue(totBuyPrice * 0.0075);
+            const sellBrokerage = (totSellPrice > 9259.0) ?
+                RoundOffValue(totSellPrice * 0.0027) :
+                (totSellPrice > 3333.33) ? 25 : RoundOffValue(totSellPrice * 0.0075);
+            const totalBrokerage = buyBrokerage + sellBrokerage;
+            const payOut = totSellPrice - totBuyPrice;
+            const etc = RoundOffValue((totBuyPrice + totSellPrice) * 0.000034);
+            const sebi = RoundOffValue((totBuyPrice + totSellPrice) * 0.0000005);
+            const gst = RoundOffValue((totalBrokerage + etc + sebi) * 0.09);
+            const totalGST = gst * 2;
+            const sd = 0;
+            const stt = RoundOffValue((totSellPrice + totBuyPrice) * 0.001);
+            const totalDed = RoundOffValue(totalBrokerage + etc + sebi + totalGST + stt + sd);
+            const netAmount = payOut - totalDed;
+            document.getElementById('NetAmount').value = netAmount;
+            document.getElementById('AmountRequired').value = totBuyPrice;
+            document.getElementById('TotalDeduction').value = totalDed;
+            document.getElementById('PayLabel').innerHTML = (netAmount > 0) ? 'Payout Obligation' : 'Pay in Obligation';
+            document.getElementById('PayOut').value = payOut;
+            document.getElementById('Brokerage').value = totalBrokerage;
+            document.getElementById('ETC').value = etc;
+            document.getElementById('SEBI').value = sebi;
+            document.getElementById('SGST').value = gst;
+            document.getElementById('CGST').value = gst;
+            document.getElementById('GST').value = totalGST;
+            document.getElementById('SD').value = sd;
+            document.getElementById('STT').value = stt;
+        }
     }
 }
 
@@ -47,5 +86,11 @@ function RoundOffValue(val) {
 $(window).on('load', function () {
     $('#BuyQuantity').keyup(function () {
         $('#SellQuantity').val(this.value);
+    });
+    $('#intraday').change(function(){
+        document.getElementById('positional').checked = ! $(this).is(":checked");
+    });
+    $('#positional').change(function(){
+        document.getElementById('intraday').checked = ! $(this).is(":checked");
     });
 });
